12702f9f2469f4951e82870424d10acdc6ec0159
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-01 04:17:41 +0800

    Making our keplar data render the 8 habitable planets by react front end under Exoplanet

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,10 +1,11 @@
-const planets = require('../../models/planets.model');
+// node uses this require model function to process the model data
+const { planets }= require('../../models/planets.model');
 
-//controller functions to get all planets
+//controller functions to get all planets request and response
 function getAllPlanets(req, res ) {
   res.status(200).json(planets);
 }
 
 module.exports = {
-  getAllPlanets
+  getAllPlanets,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 //importing the app object
 const app = require('./app');
 
+//importing the data model into server
+const {loadPlanetsData} = require('./models/planets.model'); 
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,8 +12,15 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 
-//callback listening function
-server.listen(PORT, () => {
-   console.log(`Listening on port ${PORT}...`);
-});
+//must include await function inside async
+async function startServer() {
+    await loadPlanetsData();
+    //callback listening function
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+} 
+startServer();
+
+
 
