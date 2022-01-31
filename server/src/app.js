8e79842410398b2d4c7a2ea6ce6b26c23ceff950
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-01 05:52:17 +0800

    Enabling express js render both the client and server local ports simultaneously

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 //***All express JS & Json middleware codes live here
 //express object import 
 const express = require('express');
+// we need path to serve the express middlware that joins both client and sever together in the public so we can run server in root directory
+const path = require('path');
 
 //import cors middleware
+
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
@@ -16,8 +19,18 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+//express middlewares => thr path.join will make express middleware work on localhost:8000
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+
 //middleware to get planetRoutes
 app.use(planetsRouter);
+
+
+
+
+
 
 module.exports = app;
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,5 +1,5 @@
 // node uses this require model function to process the model data
-const { planets }= require('../../models/planets.model');
+const { planets } = require('../../models/planets.model');
 
 //controller functions to get all planets request and response
 function getAllPlanets(req, res ) {
