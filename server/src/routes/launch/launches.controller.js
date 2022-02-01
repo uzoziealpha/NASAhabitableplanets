099005de6e6a125b7c96eaaad4150dce4577cf46
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-01 11:16:06 +0800

    Included isNaN to convert missing figures or properties and render to us the missing handlers

--- server/src/routes/launch/launches.controller.js
+++ server/src/routes/launch/launches.controller.js
@@ -17,7 +17,29 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
     // the date should be in this format to accept all dates
+    
+ //******* How the API fixes missing data *_********
+// the if statement states if launch mission is missing ||(or) ..... 
+//then respond with a response of status 400.  
+// ****SERVER COMPLAINS FOR BAD REQUEST
+    if (!launch.mission || !launch.rocket || !launch.launchDate 
+        || !launch.destination) {
+           return res.status(400).json({
+               error: 'Missing required launch props',
+           });
+        }
+
+
+    
     launch.launchDate = new Date(launch.launchDate);
+    //***** This if statement function checks if the date is valid or not */
+    //using isNan = not a number
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
+
     addNewLaunch(launch);
     //the controller withh return in the Network header 
     return res.status(201).json(launch);
