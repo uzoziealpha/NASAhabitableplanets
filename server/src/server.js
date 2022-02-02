0fc053bbd5874d7628291b7f529dd803c5f52f5a
Obinna Uzozie <Uzozie.obi@icloud.com>
2022-02-02 10:07:14 +0800

    connecting mongoose to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 //importing the app object
 const app = require('./app');
@@ -6,14 +7,29 @@ const app = require('./app');
 //importing the data model into server
 const {loadPlanetsData} = require('./models/planets.model'); 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 //http server
 const server = http.createServer(app);
 
+//pasting mongoURL   changing the first name to whatever 
+const MONGO_URL = 'mongodb+srv://nasa-api:9fmqWw4x4spsafaX@cluster0.ealox.mongodb.net/nasa?retryWrites=true&w=majority'
 
-//must include await function inside async
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connecting ready!');
+})
+
+//declare mongoose error 
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
+
+
+
+//must include await function inside async for mongoose and datas
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
     //callback listening function
     server.listen(PORT, () => {
